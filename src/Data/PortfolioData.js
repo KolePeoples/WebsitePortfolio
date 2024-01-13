4900c47d67063424e1011931751260455f83f78e
kolepeoples <kolepeoples@gmail.com>
2024-01-13 11:19:18 -0600

    Update tools

--- src/Data/PortfolioData.js
+++ src/Data/PortfolioData.js
@@ -2,7 +2,7 @@ export default [
     {
         title: "Weekly Workouts",
         imgUrl: "src/assets/wwbackground.png",
-        stack: ['Tailwind', 'React', 'Vite'],
+        stack: ['Java', 'XML', 'Android Studio'],
         link: "https://github.com/KolePeoples/WeeklyWorkoutsFinal/tree/main/WeeklyWorkouts%20App",
     },
     {
