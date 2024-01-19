2ae98c1361f3e6a3d63090252384ffb96d5de79e
kolepeoples <kolepeoples@gmail.com>
2024-01-19 13:00:22 -0600

    Add and style images

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,6 +6,7 @@ function Intro()
         <div className="flex items-center 
         justify-center flex-col text-center pt-20
         pb-6">
+            <img src="/assets/Headshot.png" style={{width: 200, height: 200, borderRadius: 200/ 2}}></img>
             <h1 className="text-4xl md: text-7xl dark:text-white mb-1 md: mb-3 font-bold">Kole Peoples</h1>
             <p className="text-base md:text-xl mb-3 font-medium">Software and Mobile App Developer</p>
             <p className="text-sm max-w-xl mb-6 font-bold">I built this website to highlight some of my projects as a developer. 
@@ -15,5 +16,5 @@ function Intro()
         </div>
     )
 }
-
+//  style={{width: 400, height: 400, borderRadius: 400/ 2}}
 export default Intro;
